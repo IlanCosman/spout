c78c83d7a84ee6ac1b66d9afad812f8134197430
Ilan Cosman <ilancosman@gmail.com>
2020-09-10 20:56:27 -0700

    Use universal variables, optimizations

--- functions/@test.fish
+++ functions/@test.fish
@@ -1,16 +1,13 @@
 function @test -a name
     set testArgs $argv[2..-1]
 
-    # Ordered by how common tests are, given that we search iteratively through the list
-    set -a operatorList = != -{n,z} # Strings
-    set -a operatorList -{eq,ne,gt,ge,lt,le} # Numbers
-    set -a operatorList -{b,c,d,e,f,g,G,k,L,O,p,r,s,S,t,u,w,x} # Files and directories
-
-    for op in $operatorList
+    for op in $_spout_operators
         if set -l operatorIndex (contains --index -- $op $testArgs)
             set operator $op
 
-            if not set expected (_spout_operator_expectations)
+            if set -l index (contains --index -- $op $_spout_operators_with_expectations)
+                set expected $_spout_expectations[$index]
+            else
                 set expected $testArgs[(math $operatorIndex+1)]
             end
 
@@ -30,61 +27,14 @@ function @test -a name
         set _spout_test_number_passed (math $_spout_test_number_passed +1)
         return 0
     else
-        echo "not ok $_spout_test_number $name"
-        echo "  ---"
-        echo "    operator: $operator"
-        echo "    expected: $expected"
-        echo "    actual:   $actual"
-        echo "  ..."
+        echo "not ok $_spout_test_number $name
+  ---
+    operator: $operator
+    expected: $expected
+    actual:   $actual
+  ..."
 
         set _spout_test_number_failed (math $_spout_test_number_failed +1)
         return 1
-    end
-end
-
-function _spout_operator_expectations --no-scope-shadowing
-    switch $operator
-        case '-n'
-            echo "a non-zero length string"
-        case '-z'
-            echo "a zero length string"
-        case '-b'
-            echo "a block device"
-        case '-c'
-            echo "a character device"
-        case '-d'
-            echo "a directory"
-        case '-e'
-            echo "an existing file"
-        case '-f'
-            echo "a regular file"
-        case '-g'
-            echo "a file with the set-group-ID bit set"
-        case '-G'
-            echo "a file with same group ID as the current user"
-        case '-k'
-            echo "a file with the sticky bit set"
-        case '-L'
-            echo "a symbolic link"
-        case '-O'
-            echo "a file owned by the current user"
-        case '-p'
-            echo "a named pipe"
-        case '-r'
-            echo "a file marked as readable"
-        case '-s'
-            echo "a file of size greater than zero"
-        case '-S'
-            echo "a socket"
-        case '-t'
-            echo "a terminal tty file descriptor"
-        case '-u'
-            echo "a file with the set-user-ID bit set"
-        case '-w'
-            echo "a file marked as writable"
-        case '-x'
-            echo "a file marked as executable"
-        case '*'
-            return 1
     end
 end
--- functions/spout.fish
+++ functions/spout.fish
@@ -1,7 +1,36 @@
 function spout
-    set -lx _spout_test_number 0
-    set -lx _spout_test_number_passed 0
-    set -lx _spout_test_number_failed 0
+    set -U _spout_operators
+    set -a _spout_operators = != -{n,z} # Strings
+    set -a _spout_operators -{eq,ne,gt,ge,lt,le} # Numbers
+    set -a _spout_operators -{b,c,d,e,f,g,G,k,L,O,p,r,s,S,t,u,w,x} # Files and directories
+
+    set -U _spout_operators_with_expectations -{n,z,b,c,d,e,f,g,G,k,L,O,p,r,s,S,t,u,w,x}
+
+    set -U _spout_expectations \
+        "a non-zero length string" \
+        "a zero length string" \
+        "a block device" \
+        "a character device" \
+        "a directory" \
+        "an existing file" \
+        "a regular file" \
+        "a file with the set-group-ID bit set" \
+        "a file with same group ID as the current user" \
+        "a file with the sticky bit set" \
+        "a symbolic link" \
+        "a file owned by the current user" \
+        "a named pipe" \
+        "a file marked as readable" \
+        "a file of size greater than zero" \
+        "a socket" \
+        "a terminal tty file descriptor" \
+        "a file with the set-user-ID bit set" \
+        "a file marked as writable" \
+        "a file marked as executable"
+
+    set -U _spout_test_number 0
+    set -U _spout_test_number_passed 0
+    set -U _spout_test_number_failed 0
 
     echo 'TAP version 13'
 
@@ -9,12 +38,21 @@ function spout
         fish $file
     end
 
-    echo
-    echo "1..$_spout_test_number"
-    echo "# pass $_spout_test_number_passed"
-    echo "# fail $_spout_test_number_failed"
+    echo "
+1..$_spout_test_number
+# pass $_spout_test_number_passed
+# fail $_spout_test_number_failed"
 
     if test $_spout_test_number_failed -gt 0
         return 1
     end
+end
+
+function _spout_uninstall --on-event _spout_uninstall
+    set -e _spout_operators
+    set -e _spout_operators_with_expectations
+    set -e _spout_expectations
+    set -e _spout_test_number
+    set -e _spout_test_number_passed
+    set -e _spout_test_number_failed
 end
