123cae0d025e94f5462938357319a7cbb43db34b
Ilan Cosman <ilancosman@gmail.com>
2020-10-09 14:52:35 -0700

    blah

---
+++ conf.d/spout_events.fish
@@ -0,0 +1,11 @@
+function spout_events_install --on-event spout_events_install
+    echo "Thanks for installing spout!"
+end
+
+function spout_events_update --on-event spout_events_update
+    echo "Welcome to your new spout."
+end
+
+function spout_events_remove --on-event spout_events_remove
+    echo "Sad to see you go :("
+end
